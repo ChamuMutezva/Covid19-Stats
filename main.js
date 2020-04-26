976329a9771ca7fa18cd22effff372e11e2fd8e0
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-04-26 13:09:17 +0200

    google font mula imported and css file creation

--- main.js
+++ main.js
@@ -2,12 +2,25 @@ async function covidData () {
     let response = await fetch('https://api.covid19api.com/summary');
     let results = await response.json();
     const {Global, Countries} = results;
-    console.log(Global.NewRecovered);
+    console.log(Global);
     console.log(Countries)
-    Countries.forEach(country => {
-        console.log(country.Country);
-        
-    });
+   // Countries.forEach(country => {
+   //     console.log(country.Country);        
+   // });
+
+    const newConfirmed = document.querySelector(".newConfirmed");
+    const newDeath = document.querySelector(".newDeath");
+    const newRecovered = document.querySelector(".newRecovered");
+    const totalConfirmedCases = document.querySelector(".totalConfirmed");
+    const totalDeath = document.querySelector(".totalDeath");
+    const totalRecovered = document.querySelector(".totalRecovered");
+    newConfirmed.innerHTML = Global.NewConfirmed;
+    newDeath.innerHTML = Global.NewDeaths;
+    newRecovered.innerHTML = Global.NewRecovered;
+    totalConfirmedCases.innerHTML = Global.TotalConfirmed;
+    totalDeath.innerHTML = Global.TotalDeaths;
+    totalRecovered.innerHTML = Global.TotalRecovered;
+
 }
 
 covidData();
