b1313c2cd10c161418fd2661781a894a405e3cb8
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-04-26 14:07:54 +0200

    initial main template

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ async function covidData () {
     let results = await response.json();
     const {Global, Countries} = results;
     console.log(Global);
-    console.log(Countries)
+    console.log(Countries);
+    const timeExtracted = Countries[0].Date;
+    console.log(timeExtracted)
    // Countries.forEach(country => {
    //     console.log(country.Country);        
    // });
@@ -14,13 +16,14 @@ async function covidData () {
     const totalConfirmedCases = document.querySelector(".totalConfirmed");
     const totalDeath = document.querySelector(".totalDeath");
     const totalRecovered = document.querySelector(".totalRecovered");
+    const dateLabel = document.querySelector(".dateTaken");
     newConfirmed.innerHTML = Global.NewConfirmed;
     newDeath.innerHTML = Global.NewDeaths;
     newRecovered.innerHTML = Global.NewRecovered;
     totalConfirmedCases.innerHTML = Global.TotalConfirmed;
     totalDeath.innerHTML = Global.TotalDeaths;
     totalRecovered.innerHTML = Global.TotalRecovered;
-
+    dateLabel.innerHTML = timeExtracted;
 }
 
 covidData();
