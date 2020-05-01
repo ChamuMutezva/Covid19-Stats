b3aa405e40953a926bbf5035910edf9b0e17bab9
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-05-01 12:28:05 +0200

    global display output fix

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ searchCountry.addEventListener("keyup", (event) => {
     const allCountries = Array.from(document.querySelectorAll(".countryName"));
     console.log(event.key)
     console.log(searchCountry.value)
-   
+
     allCountries.forEach(countrySelect => {
-       const countries = countrySelect.innerHTML.toLowerCase();        
-        if(countries.includes(searchCountry.value.toLowerCase().trim())) {
+        const countries = countrySelect.innerHTML.toLowerCase();
+        if (countries.includes(searchCountry.value.toLowerCase().trim())) {
             console.log(countrySelect.parentElement);
             console.log(countries);
             countrySelect.parentElement.style.display = "block";
@@ -24,14 +24,14 @@ searchCountry.addEventListener("keyup", (event) => {
 
 })
 
-menuOpen.addEventListener("click", ()=> {
+menuOpen.addEventListener("click", () => {
     console.log("menu active")
     menuClose.classList.toggle("menuToggle");
     menuOpen.classList.toggle("menuToggle");
     modal.classList.toggle("menuToggle");
 })
 
-menuClose.addEventListener("click", ()=> {
+menuClose.addEventListener("click", () => {
     menuClose.classList.toggle("menuToggle");
     menuOpen.classList.toggle("menuToggle");
     modal.classList.toggle("menuToggle");
@@ -42,36 +42,50 @@ async function covidData() {
     const response = await fetch('https://api.covid19api.com/summary');
     const results = await response.json();
     const { Global, Countries } = results;
-   // console.log(Global);
-   // console.log(Countries);
+    // console.log(Global);
+    // console.log(Countries);
     const timeExtracted = Countries[0].Date;
-   // console.log(timeExtracted)
+    // console.log(timeExtracted)
     const currentDateTime = timeExtracted.split("T");
-  // console.log(currentDateTime);
-   const dateToday = currentDateTime[0];
-   const timeToday = currentDateTime[1].substring(0, currentDateTime[1].length - 1);
-  // console.log(dateToday);
-  // console.log(timeToday);
-    let countryHolder = document.querySelector(".countryState");   
-    
-    
+    // console.log(currentDateTime);
+    const dateToday = currentDateTime[0];
+    const timeToday = currentDateTime[1].substring(0, currentDateTime[1].length - 1);
+    // console.log(dateToday);
+    // console.log(timeToday);
+    let countryHolder = document.querySelector(".countryState");
+
+
     Countries.forEach(country => {
-       // console.log(country.Country);
+        const divCountry = document.createElement("div");
+       
+        // console.log(country.Country);
         //create a child element to hold all countries 
-     let divCountry = document.createElement("div");  
-     countryHolder.appendChild(divCountry);   
-    let countryName = document.createElement("h3");
-    countryName.classList.add("countryName");
-    let countryDetail = document.createElement("span");
-    divCountry.classList.add("countryCard")
-    countryName.innerHTML = `${country.Country} `
-    countryDetail.innerHTML = `New Confirmed: ${country.NewConfirmed} <br/> 
+        //create a button
+
+        const btn = document.createElement("button");
+        btn.classList.add("btn");
+        btn.innerHTML = "Enter";      
+
+        countryHolder.appendChild(divCountry);
+
+        let countryName = document.createElement("h3");
+        countryName.classList.add("countryName");
+
+        let countryDetail = document.createElement("span");
+        divCountry.classList.add("countryCard")
+
+        countryName.innerHTML = `${country.Country} `
+        countryDetail.innerHTML = `New Confirmed: ${country.NewConfirmed} <br/> 
     New Death: ${country.NewDeaths} <br/> New Recovered: ${country.NewRecovered} <br/>    
     Total Confirmed: ${country.TotalConfirmed} <br/>  
     Total Deaths: ${country.TotalDeaths} <br/>
     Total Recovered: ${country.TotalRecovered}`;
-    divCountry.appendChild(countryName);
-    divCountry.appendChild(countryDetail);
+
+        //add children to parent
+        divCountry.appendChild(countryName);
+        divCountry.appendChild(countryDetail);
+        divCountry.appendChild(btn);
+
     });
 
     const newConfirmed = document.querySelector(".newConfirmed");
@@ -82,23 +96,23 @@ async function covidData() {
     const totalRecovered = document.querySelector(".totalRecovered");
     const dateLabel = document.querySelector(".dateTaken");
 
-    
+
     newConfirmed.innerHTML = Global.NewConfirmed;
     newDeath.innerHTML = Global.NewDeaths;
     newRecovered.innerHTML = Global.NewRecovered;
     totalConfirmedCases.innerHTML = Global.TotalConfirmed;
     totalDeath.innerHTML = Global.TotalDeaths;
     totalRecovered.innerHTML = Global.TotalRecovered;
-    dateLabel.innerHTML = `${dateToday}  ${timeToday}`;    
+    dateLabel.innerHTML = `${dateToday}  ${timeToday}`;
 
 }
 
 covidData();
 
 // api testing - not used in project
-async function testData () {
+async function testData() {
     const response = await fetch("https://api.covid19api.com/countries");
     const results = await response.json();
-console.log(results)
+    console.log(results)
 }
 testData()
