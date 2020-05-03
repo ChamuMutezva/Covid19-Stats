c11bc691c90b9d82959619bd74344f860d901b13
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-05-03 10:50:54 +0200

    feat: intro to country select implementation

--- main.js
+++ main.js
@@ -51,77 +51,84 @@ menuClose.addEventListener("click", () => {
 //function to fetch data from the Covid19 Api
 //Data is dynamic and is subject to change as new data come in
 async function covidData() {
-    try {    
-    const response = await fetch('https://api.covid19api.com/summary');
-    const results = await response.json();
-    const { Global, Countries } = results;
-    // console.log(Global);
-    // console.log(Countries);
-    const timeExtracted = Countries[0].Date;
-    // console.log(timeExtracted)
-    const currentDateTime = timeExtracted.split("T");
-    // console.log(currentDateTime);
-    const dateToday = currentDateTime[0];
-    const timeToday = currentDateTime[1].substring(0, currentDateTime[1].length - 1);
-    // console.log(dateToday);
-    // console.log(timeToday);
-    let countryHolder = document.querySelector(".countryState");
+    try {
+        const response = await fetch('https://api.covid19api.com/summary');
+        const results = await response.json();
+        const { Global, Countries } = results;
+        // console.log(Global);
+        // console.log(Countries);
+        const timeExtracted = Countries[0].Date;
+        // console.log(timeExtracted)
+        const currentDateTime = timeExtracted.split("T");
+        // console.log(currentDateTime);
+        const dateToday = currentDateTime[0];
+        const timeToday = currentDateTime[1].substring(0, currentDateTime[1].length - 1);
+        // console.log(dateToday);
+        // console.log(timeToday);
+        let countryHolder = document.querySelector(".countryState");
 
 
-    Countries.forEach(country => {
-        const divCountry = document.createElement("div");
-       
-        // console.log(country.Country);
-        //create a child element to hold all countries 
-        //create a button
+        Countries.forEach(country => {
+            const divCountry = document.createElement("div");
 
-        const btn = document.createElement("button");
-        btn.classList.add("btn");
-        btn.innerHTML = "Enter";      
+            // console.log(country.Country);
+            //create a child element to hold all countries 
+            //create a button
 
-        countryHolder.appendChild(divCountry);
+            const btn = document.createElement("button");
+            btn.classList.add("btn");
+            btn.innerHTML = "Read more...";
 
-        let countryName = document.createElement("h3");
-        countryName.classList.add("countryName");
+            countryHolder.appendChild(divCountry);
 
-        let countryDetail = document.createElement("span");
-        divCountry.classList.add("countryCard")
+            let countryName = document.createElement("h3");
+            countryName.classList.add("countryName");
 
-        countryName.innerHTML = `${country.Country} `
-        countryDetail.innerHTML = `New Confirmed: ${country.NewConfirmed.toLocaleString()} <br/> 
+            let countryDetail = document.createElement("span");
+            divCountry.classList.add("countryCard")
+
+            countryName.innerHTML = `${country.Country} `
+            countryDetail.innerHTML = `New Confirmed: ${country.NewConfirmed.toLocaleString()} <br/> 
     New Death: ${country.NewDeaths.toLocaleString()} <br/> New Recovered: ${country.NewRecovered} <br/>    
     Total Confirmed: ${country.TotalConfirmed.toLocaleString()} <br/>  
     Total Deaths: ${country.TotalDeaths.toLocaleString()} <br/>
     Total Recovered: ${country.TotalRecovered.toLocaleString()}`;
 
-        //add children to parent
-        divCountry.appendChild(countryName);
-        divCountry.appendChild(countryDetail);
-        divCountry.appendChild(btn);
+            //add children to parent
+            divCountry.appendChild(countryName);
+            divCountry.appendChild(countryDetail);
+            divCountry.appendChild(btn);
 
-    });
+            //add eventlistener to button
+            btn.addEventListener("click", (evt) => {
+                console.log(`button clicked is ${country.Country}`);
+                alert(`${country.Country} information will 
+            be implemented soon!`);
+            })
 
-    const newConfirmed = document.querySelector(".newConfirmed");
-    const newDeath = document.querySelector(".newDeath");
-    const newRecovered = document.querySelector(".newRecovered");
-    const totalConfirmedCases = document.querySelector(".totalConfirmed");
-    const totalDeath = document.querySelector(".totalDeath");
-    const totalRecovered = document.querySelector(".totalRecovered");
-    const dateLabel = document.querySelector(".dateTaken");
+        });
+
+        const newConfirmed = document.querySelector(".newConfirmed");
+        const newDeath = document.querySelector(".newDeath");
+        const newRecovered = document.querySelector(".newRecovered");
+        const totalConfirmedCases = document.querySelector(".totalConfirmed");
+        const totalDeath = document.querySelector(".totalDeath");
+        const totalRecovered = document.querySelector(".totalRecovered");
+        const dateLabel = document.querySelector(".dateTaken");
 
 
-    newConfirmed.innerHTML = Global.NewConfirmed.toLocaleString();
-    newDeath.innerHTML = Global.NewDeaths.toLocaleString();
-    newRecovered.innerHTML = Global.NewRecovered.toLocaleString();
-    totalConfirmedCases.innerHTML = Global.TotalConfirmed.toLocaleString();
-    totalDeath.innerHTML = Global.TotalDeaths.toLocaleString();
-    totalRecovered.innerHTML = Global.TotalRecovered.toLocaleString();
-    console.log(dateToday)
-    dateLabel.innerHTML = `Data captured at: ${dateToday}  ${timeToday}`;
-    } catch(err) {
+        newConfirmed.innerHTML = Global.NewConfirmed.toLocaleString();
+        newDeath.innerHTML = Global.NewDeaths.toLocaleString();
+        newRecovered.innerHTML = Global.NewRecovered.toLocaleString();
+        totalConfirmedCases.innerHTML = Global.TotalConfirmed.toLocaleString();
+        totalDeath.innerHTML = Global.TotalDeaths.toLocaleString();
+        totalRecovered.innerHTML = Global.TotalRecovered.toLocaleString();
+        console.log(dateToday)
+        dateLabel.innerHTML = `Data captured at: ${dateToday}  ${timeToday}`;
+    } catch (err) {
         const dateLabel = document.querySelector(".dateTaken");
         dateLabel.classList.add("dataError");
-       // alert(err)
+        // alert(err)
         dateLabel.innerHTML = `Error: ${err} <br/>
         Data can only be fetched online`
     }
