14eb33a6d41547897cff700ce8490e2a09103bd9
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-05-17 12:12:19 +0200

    feat: activate, install and fetch events implemented

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,12 @@
 const filesToCache = [
     '/',
     'styles.css',
-    'images/still_life_medium.jpg',
     'index.html',
     'about.html',
     'myth.html',
     'prevention.html',
     'output.css',
-    'app.webmanifest',
+    'manifest.json',
     'images/7.png',
     'images/24.png',
     'images/26.png',
@@ -31,25 +30,63 @@ const filesToCache = [
     'images/mythbusters-27.png',
     'images/mythbusting15.png',
     'images/world.png'
-    
-  ];
-  
-  const staticCacheName = 'pages-cache-v1';
-  
-  self.addEventListener('install', event => {
+
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
     console.log('Attempting to install service worker and cache static assets');
     event.waitUntil(
-      caches.open(staticCacheName)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-      })
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
     );
-  });
+});
 
 self.addEventListener('activate', event => {
-    console.log('Service worker activating...');
+    console.log('Activating new service worker...');
+
+    const cacheWhitelist = [staticCacheName];
+
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-    console.log('Fetching:', event.request.url);
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request)
+
+                    .then(response => {
+                        // TODO 5 - Respond with custom 404 page
+                        return caches.open(staticCacheName).then(cache => {
+                            cache.put(event.request.url, response.clone());
+                            return response;
+                        });
+                    });
+
+            }).catch(error => {
+
+                // TODO 6 - Respond with custom offline page
+
+            })
+    );
 });
